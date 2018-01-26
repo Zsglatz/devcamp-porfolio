14838eede695cc0b478dc8497e17046c47eb096c
Glatz <zsglatz@gmail.com>
2018-01-26 10:17:20 -0700

    commit

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,10 +9,11 @@ class PortfoliosController < ApplicationController
 
   def new 
     @portfolio_item = Portfolio.new 
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.required(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.required(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
